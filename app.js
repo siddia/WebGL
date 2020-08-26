8204d9b30a804b0877f491ce518d4bccb9ca8dc7
Aras <aras.siddiqui15@gmail.com>
2020-08-25 23:22:04 -0400

    upload files

---
+++ app.js
@@ -0,0 +1,82 @@
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+let house;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 500;
+
+  //Camera setup
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(-10, 10, 300);
+
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  light.position.set(50, 50, 100);
+  scene.add(light);
+  
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Model
+
+  let loader = new THREE.GLTFLoader();
+  loader.load("./house/T13.glb", function(gltf) {
+    scene.add(gltf.scene);
+    // renderer.render(scene,camera);
+    house = gltf.scene.children[0];
+    animate();
+  });
+
+  // let loader = new THREE.STLLoader();
+  // loader.load("./house/T13-room.stl", function(stl) {
+  //   scene.add(stl.scene);
+  //   renderer.render(scene,camera);
+  //   house = stl.scene.children[0];
+  //   animate();
+  // });
+
+  // let loader = new THREE.OBJLoader();
+  // loader.load("./house/tinker.obj", function(obj) {
+  //   scene.add(obj.scene);
+  //   renderer.render(scene,camera);
+  //   house = obj.scene.children[0];
+  //   animate();
+  // });
+
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  house.rotation.y += 0.005;
+  house.rotation.x += 0.0005;
+  renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
